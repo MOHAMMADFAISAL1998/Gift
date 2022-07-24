783f6a2d57c8fa45660bea8244468cbde096cf0c
MOHAMMADFAISAL1998 <56969703+MOHAMMADFAISAL1998@users.noreply.github.com>
2022-07-24 11:47:50 +0530

    Update mediaButtonJS.js

--- VDay/js/mediaButtonJS.js
+++ VDay/js/mediaButtonJS.js
@@ -3,7 +3,7 @@ function toggle(){
 }
 
 function newVideoLoad(){
-    var secondVideoFile = "https://drive.google.com/file/d/1oYMec2rICEqJ28CPort_DCx5ALq4s0UG/preview";
+    var secondVideoFile = "https://drive.google.com/file/d/13V9J_IkJT80mmBvodnXaD244HxdC0OwW/preview";
     var videoinIframe = document.getElementById('videoID');
     videoinIframe.src = secondVideoFile;
     videoinIframe.load();
